9b4c4acfd6a95e16d90a50a6068da22044e0c434
selimssevgi <selimssevgi@gmail.com>
2018-08-14 22:48:20 +0300

    init project

---
+++ src/com/sercan/ay/course1/Test.java
@@ -0,0 +1,7 @@
+package com.sercan.ay.course1;
+
+public class Test {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
---
+++ src/com/sercan/ay/course1/package-info.java
@@ -0,0 +1,32 @@
+/**
+ * package
+ *
+ * java -> jvm
+ * javac -> java compiler
+ * .java -> extension of java source code
+ * .class -> compiled java code
+ *
+ * Python -> interpreted language
+ * C -> compiled language
+ * Java -> compiled + interpreted language
+ *
+ * machine code/instruction -> for cpu
+ *
+ * portable
+ *
+ * C source code -> intel compiler -> (machine code) program runs on intel cpu
+ * C source code -> amd compiler -> program runs on amd cpu
+ *
+ * Java source code -> compiler -> does not output machine code, output byte code
+ *
+ * jvm -> java virtual machine
+ * byte code (.class) -> jvm -> interprets byte code -> (run time) machine code
+ *
+ * text editor(java source code) -> javac Test.java -> Byte code(.class) -> java(jvm) Test -> machine code
+ *
+ *
+ * IDE -> Integrated Development Environment
+ * text editor + syntax highlighting + compiler + debugging + refactoring +
+ */
+
+package com.sercan.ay.course1;
